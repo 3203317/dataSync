716936b2ea01249bf3837897512958473834031e
speedt <3203317@qq.com>
2018-03-21 21:04:57 +0800

    haha

---
+++ pub/src/main/java/net/foreworld/yx/RunWsServer.java
@@ -0,0 +1,33 @@
+package net.foreworld.yx;
+
+import javax.annotation.Resource;
+
+import net.foreworld.yx.server.WsServer;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+/**
+ *
+ * @author huangxin <3203317@qq.com>
+ *
+ */
+@SpringBootApplication
+@ComponentScan("net.foreworld")
+public class RunWsServer implements CommandLineRunner {
+
+	@Resource(name = "wsServer")
+	private WsServer wsServer;
+
+	public static void main(String[] args) {
+		SpringApplication.run(RunWsServer.class, args);
+	}
+
+	public void run(String... strings) throws InterruptedException {
+		wsServer.start();
+		Thread.currentThread().join();
+	}
+
+}
---
+++ pub/src/main/java/net/foreworld/yx/util/Constants.java
@@ -0,0 +1,10 @@
+package net.foreworld.yx.util;
+
+/**
+ *
+ * @author huangxin <3203317@qq.com>
+ *
+ */
+public interface Constants {
+	static final String OK = "OK";
+}
